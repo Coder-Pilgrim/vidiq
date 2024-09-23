a66d3e11983a9972e58e35d792513692c46738c0
Enes İnce <81353748+Coder-Pilgrim@users.noreply.github.com>
2024-09-23 15:40:32 +0300

    add sidebar, navbar

--- app/(root)/meeting/[id]/page.tsx
+++ app/(root)/meeting/[id]/page.tsx
@@ -1,7 +1,8 @@
 import React from "react";
 
-const Meeting = () => {
-  return <div>Meeting</div>;
+const Meeting = ({params}: {params: {id: string}}) => {
+
+  return <div>Meeting Room: #{params.id}</div>;
 };
 
 export default Meeting;
